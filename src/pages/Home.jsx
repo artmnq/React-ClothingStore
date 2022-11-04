439d1895ca1569a3960c07c7adf85c517f23ecde
Andrii Artemenko <artemenko.andrey22@gmail.com>
2022-11-04 16:12:22 +0200

    feat: add page Cart layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,18 +16,21 @@ const Home = () => {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
   return (
     <>
-      <div className="content__top">
-        <Categories />
-        <Sort />
-      </div>
-      <h2 className="content__title">All clothing:</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <ItemBlock key={obj.id} {...obj} />)}
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">All clothing:</h2>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+            : items.map((obj) => <ItemBlock key={obj.id} {...obj} />)}
+        </div>
       </div>
     </>
   );
