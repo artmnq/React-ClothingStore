8f065f1b581c1bae51f867238f2173168850e07a
Andrii Artemenko <artemenko.andrey22@gmail.com>
2022-11-20 20:05:13 +0200

    fix: style-colors

--- src/pages/FullItem.tsx
+++ src/pages/FullItem.tsx
@@ -3,13 +3,19 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const FullItem: React.FC = () => {
-  const [item, setItem] = React.useState<{ image: string; title: string; price: number }>();
+  const [item, setItem] = React.useState<{
+    image: string;
+    title: string;
+    price: number;
+  }>();
   const { id } = useParams();
 
   React.useEffect(() => {
     async function fetchItem() {
       try {
-        const { data } = await axios.get('https://6362b47537f2167d6f6b31c3.mockapi.io/items/' + id);
+        const { data } = await axios.get(
+          'https://6362b47537f2167d6f6b31c3.mockapi.io/items/' + id
+        );
         setItem(data);
       } catch (error) {
         console.log(error, 'ERROR:(');
@@ -28,7 +34,7 @@ const FullItem: React.FC = () => {
 
   return (
     <div className="container">
-      <img src={item.image} alt="item" />
+      <img src={item.image} alt="item" width={300} />
       <h2>{item.title}</h2>
       <h4>{item.price}</h4>
     </div>
